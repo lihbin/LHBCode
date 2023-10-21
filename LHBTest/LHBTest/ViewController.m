69510214a9ef26f645f0d0c3a106865180d77278
黎红斌 <1540556917@qq.com>
2023-10-21 13:01:00 +0800

    添加一个nsobject对象

--- LHBTest/LHBTest/ViewController.m
+++ LHBTest/LHBTest/ViewController.m
@@ -17,6 +17,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     NSLog(@"-----哈哈哈哈");
+    NSObject *obj = [[NSObject alloc] init];
+    NSLog(@"--%@",obj);
 }
 
 
